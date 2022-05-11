35616a6206cb76862ea8fe77420d5f3c408aba73
siddik <spolatde@gmail.com>
2022-05-11 02:24:16 +0200

    react is created

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        hello weather app
+      </header>
+    </div>
+  );
+}
+
+export default App;
